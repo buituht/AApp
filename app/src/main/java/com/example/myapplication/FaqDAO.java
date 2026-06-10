691e25196b0596e49ccf5fbc7814886fd9681006
buituht <buituht@gmail.com>
2026-06-10 15:51:43 +0700

    Update FaqDAO.java

--- app/src/main/java/com/example/myapplication/FaqDAO.java
+++ app/src/main/java/com/example/myapplication/FaqDAO.java
@@ -5,12 +5,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
-import java.util.List;
 
 public class FaqDAO {
     private FirebaseFirestore firestore;
     private CollectionReference faqsRef;
-    private DatabaseHelper dbHelper;
 
     public FaqDAO() {
         firestore = FirebaseFirestore.getInstance();
@@ -19,23 +17,10 @@ public class FaqDAO {
 
     public FaqDAO(Context context) {
         this();
-        this.dbHelper = new DatabaseHelper(context);
-    }
-
-    public List<Faq> getAllFaqsSQLite() {
-        return dbHelper != null ? dbHelper.getAllFaqs() : null;
-    }
-
-    public Task<Void> addFaqFirebase(Faq faq) {
-        return addFaq(faq);
-    }
-
-    public Task<QuerySnapshot> getAllFaqsFirebase() {
-        return faqsRef.get();
     }
 
     public Task<QuerySnapshot> getAllFaqs() {
-        return getAllFaqsFirebase();
+        return faqsRef.get();
     }
 
     public Task<Void> addFaq(Faq faq) {
